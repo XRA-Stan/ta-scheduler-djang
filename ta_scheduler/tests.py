658bf4564b17b2ce362e6bd7aa4cf7e789e02a5a
Lilbang <ksalem@uwm.edu>
2025-04-18 04:27:54 -0500

    I added tests for unsuccessful logins

--- ta_scheduler/tests.py
+++ ta_scheduler/tests.py
@@ -5,19 +5,19 @@ from django.urls import reverse
 
 
 # Create your tests here.
-class successLogin(TestCase):
+class SuccessLogin(TestCase):
     def setUp(self):
         #creation of a test user
         self.username = 'testuser'
         self.password = 'test123'
         self.user = User.objects.create_user(username=self.username, password=self.password)
 
-    def testloginGood(self):
+    def testLoginGood(self):
         #self.client.login will return true or false if the user was logged in or not
         success = self.client.login(username=self.username, password=self.password)
         self.assertTrue(success)
 
-    def testloginRedirect(self):
+    def testLoginRedirect(self):
         #uses urls.py to see if it can reverse from its page back to the login
         response = self.client.post(reverse('login'), {'username': self.username, 'password': self.password})
         #This will check if a redirect occured AKA redirecting to the home page
@@ -28,10 +28,41 @@ class successLogin(TestCase):
         #checks if the user is logged in and wasnt just redirected
         self.assertTrue(user.is_authenticated)
 
-    def testloadCorrectly(self):
+    def testLoadCorrectly(self):
         #This refers to the homepage
         response = self.client.get("/")
         #This checks if the home page was loaded correctly
         self.assertEqual(response.status_code, 200)
+
+class FailedLogin(TestCase):
+    def setUp(self):
+        self.username = 'testuser'
+        self.password = 'test123'
+        self.user = User.objects.create_user(username=self.username, password=self.password)
+
+    def testWrongUsername(self):
+        login = self.client.login(username= 'wrongUsername', password=self.password)
+        self.assertFalse(login)
+
+    def testWrongPassword(self):
+        login = self.client.login(username= self.username, password= "wrongPassword")
+        self.assertFalse(login)
+
+    def testNoRedirect(self):
+        response = self.client.post(reverse('login'), {'username': "wrong", 'password': self.password})
+        self.assertNotEqual(response.status_code, 302)
+
+        user = response.context['user']
+        self.assertFalse(user.is_authenticated)
+
+    def testCapsUsername(self):
+        login = self.client.login(username= 'TESTUSER', password= self.password)
+        self.assertFalse(login)
+
+    def testCapsPassword(self):
+        login = self.client.login(username= self.username, password= "TEST123")
+        self.assertFalse(login)
+
+
 
 
